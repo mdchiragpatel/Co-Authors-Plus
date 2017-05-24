2f75fce2eb52f874a9ff023e393aabee8161dabb
Chirag Patel <chiragp@multidots.in>
2017-05-24 17:59:34 +0530

    Remove associated guest user when mapped user id deleted.

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -865,7 +865,7 @@ class CoAuthors_Plus {
 	 * @param delete_id
 	 */
 	function delete_user_action( $delete_id ) {
-		global $wpdb, $coauthors_plus;
+		global $wpdb;
 
 		$reassign_id = isset( $_POST['reassign_user'] ) ? absint( $_POST['reassign_user'] ) : false;
 
